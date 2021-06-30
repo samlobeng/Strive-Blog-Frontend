a5ad0502f0809318bff3f47b1fc43b41087e230f
Samuel Obeng <61053627+samlobeng@users.noreply.github.com>
2021-06-30 16:28:55 +0200

    fixes

--- src/components/blog/blog-item/index.jsx
+++ src/components/blog/blog-item/index.jsx
@@ -5,10 +5,12 @@ import { Link } from "react-router-dom"
 import { RiDeleteBin6Line } from "react-icons/ri"
 import "./styles.css"
 
+const apiURL = process.env.REACT_APP_BE_URL
+
 const BlogItem = (props) => {
   const deletePost = async (e) => {
     e.preventDefault()
-    const response = await fetch("http://localhost:3001/posts/" + props._id, {
+    const response = await fetch(`${apiURL}/posts/${props._id}`, {
       method: "DELETE",
     })
     if (response.ok) {
